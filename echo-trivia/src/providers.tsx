10bd8247e66d438c159c8fc45b66f46644f1092f
Brian Sproule <briansproule20@gmail.com>
2025-10-13 17:43:45 -0400

    Add light/dark mode toggle

--- echo-trivia/src/providers.tsx
+++ echo-trivia/src/providers.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { EchoProvider } from '@merit-systems/echo-next-sdk/client';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <EchoProvider config={{ appId: process.env.NEXT_PUBLIC_ECHO_APP_ID! }}>
-      {children}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
     </EchoProvider>
   );
 }
